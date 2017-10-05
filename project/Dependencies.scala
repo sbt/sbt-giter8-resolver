dd0fd4386702fde00d4cdc248274a5254c4ad755
Michael Wolfendale <michael.wolfendale@gmail.com>
2017-10-05 22:48:54 +0100

    Updated giter8 version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object Dependencies {
   lazy val templateResolverApi = "org.scala-sbt" % "template-resolver" % "0.1"
-  lazy val giter8 = "org.foundweekends.giter8" %% "giter8" % "0.7.2"
+  lazy val giter8 = "org.foundweekends.giter8" %% "giter8" % "0.10.1-SNAPSHOT"
   lazy val launcherInterface = "org.scala-sbt" % "launcher-interface" % "1.0.0-M1"
   lazy val scalactic = "org.scalactic" %% "scalactic" % "3.0.1" % "test"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1" % "test"
