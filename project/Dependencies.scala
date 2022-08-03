eb5a21579eab0dc95b01274504ba0f13be6fcb4f
Magnolia.K <magnolia.k@icloud.com>
2022-08-03 22:25:52 +0900

    Updated dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ import Keys._
 
 object Dependencies {
   lazy val templateResolverApi = "org.scala-sbt" % "template-resolver" % "0.1"
-  lazy val giter8Launcher = "org.foundweekends.giter8" %% "giter8-launcher" % "0.12.0"
-  lazy val launcherInterface = "org.scala-sbt" % "launcher-interface" % "1.1.3"
+  lazy val giter8Launcher = "org.foundweekends.giter8" %% "giter8-launcher" % "0.14.0"
+  lazy val launcherInterface = "org.scala-sbt" % "launcher-interface" % "1.3.3"
 }
